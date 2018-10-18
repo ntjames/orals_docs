c03cf929394a7691373fe509dcfdad4590c19ddf
Nathan T. James <nj1154@gmail.com>
2018-10-17 22:03:33 -0500

    update bookdown files

--- paper/compile_bookdown.R
+++ paper/compile_bookdown.R
@@ -1,3 +1,11 @@
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+out_dir<-file.path(getwd(),"output")
 
-#bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+## html gitbook
+bookdown::render_book("index.Rmd", "bookdown::gitbook", output_dir = out_dir)
+
+## pdf
+bookdown::render_book("index.Rmd", "bookdown::pdf_book", output_dir = out_dir)
+
+
+## word doc
+#bookdown::render_book("index.Rmd", "bookdown::word_document2", output_dir = out_dir)
