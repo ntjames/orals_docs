5d39634903a9f58f607d13e0ce950b8cc7a4e790
Nathan T. James <nj1154@gmail.com>
2019-01-14 20:13:28 -0600

    initial sketch/outline of paper

--- paper/compile_bookdown.R
+++ paper/compile_bookdown.R
@@ -1,11 +1,16 @@
 out_dir<-file.path(getwd(),"output")
 
+## made with bookdown: https://bookdown.org/yihui/bookdown/
+
 ## html gitbook
-bookdown::render_book("index.Rmd", "bookdown::gitbook", output_dir = out_dir)
+# bookdown::render_book("index.Rmd", "bookdown::gitbook", output_dir = out_dir)
 
 ## pdf
 bookdown::render_book("index.Rmd", "bookdown::pdf_book", output_dir = out_dir)
 
-
 ## word doc
 #bookdown::render_book("index.Rmd", "bookdown::word_document2", output_dir = out_dir)
+
+
+
+
