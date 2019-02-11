402c457ea3006bf71b2e0f98440a3eb5dfe9a9c8
Nathan T. James <nj1154@gmail.com>
2019-02-11 02:29:11 -0600

    Update auxiliary files

--- paper/compile_bookdown.R
+++ paper/compile_bookdown.R
@@ -3,7 +3,7 @@ out_dir<-file.path(getwd(),"output")
 ## made with bookdown: https://bookdown.org/yihui/bookdown/
 
 ## html gitbook
-#bookdown::render_book("index.Rmd", "bookdown::gitbook", output_dir = out_dir)
+bookdown::render_book("index.Rmd", "bookdown::gitbook", output_dir = out_dir)
 
 ## pdf
 bookdown::render_book("index.Rmd", "bookdown::pdf_book", output_dir = out_dir)
@@ -11,9 +11,6 @@ bookdown::render_book("index.Rmd", "bookdown::pdf_book", output_dir = out_dir)
 ## word doc
 #bookdown::render_book("index.Rmd", "bookdown::word_document2", output_dir = out_dir)
 
-
 #for citations
 #https://cran.r-project.org/web/packages/citr/readme/README.html
-
-
 
